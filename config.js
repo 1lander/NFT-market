38be800bf9b39f6ee7a212e2d07950cd1957935a
Lander <landervanlandeghem@hotmail.com>
2021-07-31 19:52:33 +0200

    using public network

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const nftaddress = "0x0e38A8015ca335E2E2c73BDcd001E77Bec4932ee"
-export const nftmarketaddress = "0x23175dcB49ba3b2F2561d86A195bb59ed253769d"
+export const nftmarketaddress = "0x1E3133194B8333Ac65A9079DE72B962F6546cA00"
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,9 @@
 // /* hardhat.config.js */
-// require("@nomiclabs/hardhat-waffle");
-// const fs = require("fs");
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const projectId = "eef942d247ee46a98bcf2a98fc4b021f";
-
-// module.exports = {
-//   defaultNetwork: "hardhat",
-//   networks: {
-//     hardhat: {
-//       chainId: 1337,
-//     },
-//     mumbai: {
-//       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-//       accounts: [privateKey],
-//     },
-//     mumbai: {
-//       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-//       accounts: [privateKey],
-//     },
-//   },
-//   solidity: {
-//     version: "0.8.4",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200,
-//       },
-//     },
-//   },
-// };
-
 require("@nomiclabs/hardhat-waffle");
 const fs = require("fs");
 const privateKey =
   fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+const infuraId = "eef942d247ee46a98bcf2a98fc4b021f";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -42,6 +12,8 @@ module.exports = {
       chainId: 1337,
     },
     mumbai: {
+      //infura
+      //url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
       url: "https://rpc-mumbai.matic.today",
       accounts: [privateKey],
     },
